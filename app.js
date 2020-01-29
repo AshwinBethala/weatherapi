fc3427e1735a09e30197f57c25c1335d5fd08f89
Ashwin Bethala <57239378+AshwinBethala@users.noreply.github.com>
2020-01-28 18:08:27 -0600

    New files for the Weather API
    Here is the new and improved index I created with the guidance of my tutor. In addition, I have created a .js file that has the initial layout for the javascript.

---
+++ app.js
@@ -0,0 +1,21 @@
+//console.log("hello world");
+
+
+var apiKey = `df5617d2cffd20d7b7fcfcba640e52a3`
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=${apiKey}`
+
+function getWeather () {
+    
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        var bigDiv = `<div>`
+        bigDiv.appendTo($("#containerDiv"))
+    })
+}
+
+getWeather()
